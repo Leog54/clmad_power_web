a0c713c423e4e396768664b884979c2a7b5a842e
Marianne <marianne5102@gmail.com>
2022-04-13 13:39:13 +0200

    css add.php

--- templates/Users/add.php
+++ templates/Users/add.php
@@ -5,32 +5,27 @@
  */
 ?>
 <div class="add">
+<div class="connForm">
     <h3>Inscrivez-vous</h3>
     <?= $this->Form->create($user) ?>
     <fieldset> 
         <?php
             echo "<div class='nomComplet'>";
-            echo "<div class='labelContainer'>";
             echo $this->Form->control('nom_user', ['label' => 'Nom']);
-            echo "</div>";
-            echo "<div class='labelContainer'>";
             echo $this->Form->control('prenom_user', ['label' => 'Prénom']); 
-            echo "</div>";
             echo "</div>";
             echo "<div class='civilite'>";
             echo "<label>Civilité</label>";                  
             echo $this->Form->radio('sexe_user', ['Masculin','Féminin','Neutre']);
             echo "</div>";
-            echo "<div class='labelContainer'>";
+            echo "<div class='nomComplet'>";
             echo $this->Form->control('num_rue_user', ['label' => 'Numéro de rue']);  
             echo $this->Form->control('rue_user', ['label' => 'Rue']);
             echo "</div>";
-            echo "<div class='labelContainer'>";
             echo $this->Form->control('cp_user', ['label' => 'Code Postal']);
             echo $this->Form->control('ville_user', ['label' => 'Ville']);
-            echo "</div>";
             echo $this->Form->control('num_tel_user', ['label' => 'Téléphone']);
-            echo $this->Form->control('email', ['label' => 'Email']);
+            echo $this->Form->control('email', ['label' => 'Adresse e-mail']);
             echo $this->Form->control('password', ['label' => 'Mot de passe']);
             echo $this->Form->control('date_naissance_user', ['label' => 'Date de naissance']);
             echo $this->Form->control('situation_familial_user', ['label' => 'Situation familiale']);
@@ -39,4 +34,5 @@
     </fieldset>
     <?= $this->Form->button(__("S'enregistrer")) ?>
     <?= $this->Form->end() ?>
+</div>
 </div>
