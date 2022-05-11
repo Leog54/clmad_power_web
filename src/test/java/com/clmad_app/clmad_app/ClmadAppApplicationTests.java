97bfd79e65ec1cc0876e73b1c2c3cb71c866f4e5
Dylan Burgon <dylan.burgon@outlook.fr>
2022-05-11 14:13:53 +0200

    spring

---
+++ src/test/java/com/clmad_app/clmad_app/ClmadAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clmad_app.clmad_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClmadAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
